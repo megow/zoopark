bb74365e6fe8bfd66e00200b3057e2e08284c83b
MEGANE MARESCHAL <megow@MacBook-Pro-de-MEGANE.local>
2016-07-20 21:22:08 +0200

    README & CORR | v4.dev - b.dev.home

---
+++ README.txt
@@ -0,0 +1,11 @@
+# Zoopark | exercice intégré 
+## Détails
+- auteur : meg
+- état : en cours + modifications
+## Todo
+- base de données
+- découpage
+- intégration
+- admin _ connexion
+- lang
+- modifications.css & modifications.js
--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
-	// connexion au serveur
+	//connexion au serveur
 	define("DB_USER","root");
 	define("DB_NAME", "megmeg_zoopark");
 	define("DB_HOST","localhost");
 	define("DB_PASSWORD", "root");
-	// changer les données lorsqu'on les migre vers un hebergeur
+	//changer les données lorsqu'on les migre vers un hébergeur
 
 
-	$db_connect = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); //établir la connexion au serveur
+	$db_connect = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	//établir la connexion au serveur
 	//print_r($db_connect);
 
 	if($db_connect->connect_errno) :
@@ -18,11 +19,10 @@
 
 	session_start();
 
-	/**/
+
 	define('TABLE_PREFIX','ost_');
 
 	if(isset($_GET['lang'])) :
-
 
 		switch($_GET['lang']) {
 			case 'fr':
@@ -31,16 +31,10 @@
 				break;
 
 				case 'en':
-				//echo "test";
 				$_SESSION['lang']='en';
 				$lang ='en';
 				break;
-			
-				// default:
-				// $_SESSION['lang']='fr';
-				// $_GET['lang']='fr';
-				// $lang ='fr';
-				// break;	
+				
 		}
 
 		define('LANG',$_SESSION['lang']);
